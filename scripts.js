6b22a0bbdada0d6e0151c64d63c862a4ce56ea69
BetterNerfIrelia <lauterach@gmx.at>
2020-10-20 13:23:37 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 function test() {
   alert("TEST");
 }
+
+$('input[type="radio"]').on('click change', function(e) {
+    console.log(e.type);
+	var blue = document.getElementById('jammer_blue');
+	var red = document.getElementById('jammer_red');
+	blue.style.display = "flex";
+	red.style.display = "flex";
+
+});
