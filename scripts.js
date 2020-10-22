e07271b9df3c978f69e98a6aac2bda88385f497f
BetterNerfIrelia <lauterach@gmx.at>
2020-10-22 08:21:40 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 function radioListener() {
   	var blue = document.getElementById('jammer_blue');
 	var red = document.getElementById('jammer_red');
+	var label4 = document.getElementById('label4');
+	label4.style.backgroundColor = "#2eb82e";
 	blue.style.display = "flex";
 	red.style.display = "flex";
 }
@@ -9,4 +11,3 @@ document.getElementById("radio1").addEventListener("click", radioListener);
 document.getElementById("radio2").addEventListener("click", radioListener);
 document.getElementById("radio3").addEventListener("click", radioListener);
 document.getElementById("radio4").addEventListener("click", radioListener);
-
